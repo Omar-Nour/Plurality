16cb301591ca9867bc613079222fa0b278e3a701
Omar-Nour <Omar-Nour@users.noreply.github.com>
2020-04-10 22:42:34 +0000

    automated commit by submit50

--- credit.py
+++ credit.py
@@ -7,26 +7,21 @@ elif (int(CC[:2]) >= 51) and (int(CC[:2]) <= 55) and (len(CC) == 16):
     typ = "MASTERCARD\n"
 else:
     typ = "INVALID\n"
-sum=0
+sum = 0
 if typ != "INVALID\n":
-    for i in range(len(CC)-2,-1,-2):
+    for i in range(len(CC)-2, -1, -2):
         tnum = int(CC[i])*2
         if tnum < 10:
-            sum+= tnum
+            sum += tnum
         else:    
             stnum = str(tnum)
-            sum+= int(stnum[0])
-            sum+= int(stnum[1])
-    for i in range(len(CC)-1,-1,-2):
-        sum+= int(CC[i])
+            sum += int(stnum[0])
+            sum += int(stnum[1])
+    for i in range(len(CC)-1, -1, -2):
+        sum += int(CC[i])
     if sum % 10 == 0:
         print(typ)
     else:
-        print("Algo-fail",sum)
         print("INVALID\n")
 else:
     print(typ)
-        
-        
-        
-    
