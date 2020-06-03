687740033a9c73f7e5b7c80a591d9b69645b68b4
Omar-Nour <Omar-Nour@users.noreply.github.com>
2020-06-03 23:08:56 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -136,7 +136,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 temp[row][col].rgbtRed = ensure(round((image[row][col].rgbtRed + image[row][col - 1].rgbtRed + image[row - 1][col].rgbtRed + image[row - 1][col - 1].rgbtRed) / 4.0));
             }
             //case 9
-            else
+            else if (row != 0 && col != 0 && row != height - 1 && col != width - 1)
             {
                 temp[row][col].rgbtBlue = ensure(round((image[row - 1][col - 1].rgbtBlue + image[row - 1][col].rgbtBlue + image[row - 1][col + 1].rgbtBlue + image[row][col - 1].rgbtBlue + image[row][col].rgbtBlue + image[row][col + 1].rgbtBlue + image[row + 1][col - 1].rgbtBlue + image[row + 1][col].rgbtBlue + image[row + 1][col + 1].rgbtBlue) / 9.0));
                 temp[row][col].rgbtGreen = ensure(round((image[row - 1][col - 1].rgbtGreen + image[row - 1][col].rgbtGreen + image[row - 1][col + 1].rgbtGreen + image[row][col - 1].rgbtGreen + image[row][col].rgbtGreen + image[row][col + 1].rgbtGreen + image[row + 1][col - 1].rgbtGreen + image[row + 1][col].rgbtGreen + image[row + 1][col + 1].rgbtGreen) / 9.0));
