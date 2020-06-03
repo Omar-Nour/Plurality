b41337db4140f741c67b6501723bb8df58ef72f6
Omar-Nour <Omar-Nour@users.noreply.github.com>
2020-06-03 22:08:33 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int col = 0; col < width; ++col)
         {
-            avgRGB = round((image[row][col].rgbtBlue + image[row][col].rgbtGreen + image[row][col].rgbtRed) / 3);
+            avgRGB = rint((image[row][col].rgbtBlue + image[row][col].rgbtGreen + image[row][col].rgbtRed) / 3);
             image[row][col].rgbtBlue = avgRGB;
             image[row][col].rgbtGreen = avgRGB;
             image[row][col].rgbtRed = avgRGB;
