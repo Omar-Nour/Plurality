c56013dae6b40d4fea39fd4593904453e3ea44c9
Omar-Nour <Omar-Nour@users.noreply.github.com>
2020-07-25 14:41:39 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -6,29 +6,29 @@ int main(int argc, char *argv[])
 {
     typedef uint8_t BYTE;
     char *filename = malloc(8 * sizeof(char));
-    
+
     // exit if no file
     if (argc != 2)
     {
         return argc;
     }
-    
+
     // open memory card
     FILE *memc = fopen(argv[1], "r");
     FILE *img;
     BYTE bytes[512];
-    
+
     // images found counter
     int jpg_count = -1;
     int read_bytes;
-    
+
     // to recieve return of fwrite
     int null;
-    
-    
+
+
     do
     {
-        read_bytes = fread(&bytes, 1, 512, memc);
+        read_bytes = fread(bytes, 1, 512, memc);
         if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] & 0xf0) == 0xe0)
         {
             jpg_count++;
@@ -36,30 +36,32 @@ int main(int argc, char *argv[])
             {
                 sprintf(filename, "%03i.jpg", jpg_count);
                 img = fopen(filename, "w");
-                null = fwrite(&bytes, 1, 512, img);
+                null = fwrite(bytes, 1, 512, img);
             }
             else
             {
                 fclose(img);
                 sprintf(filename, "%03i.jpg", jpg_count);
                 img = fopen(filename, "w");
-                null = fwrite(&bytes, 1, 512, img);
+                null = fwrite(bytes, 1, 512, img);
             }
-        }  
+        }
         // continue to fill next blocks of previous/opened image
         else if (jpg_count > -1)
         {
-            null = fwrite(&bytes, 1, 512, img);
+            null = fwrite(bytes, 1, 512, img);
         }
-        
+
     }
     while (read_bytes != 0);
-    
+
+    free(filename);
+
     // close img only if it was open asasan
     if (jpg_count > -1)
     {
         fclose(img);
     }
     fclose(memc);
-    
+
 }
